fe7fa37cf91f7405549ca8688f172bd793c84779
孙俊威 <sunjunwei@ECCMAC-C02VQ1QT.local>
2019-12-06 21:15:06 +0800

    feat:热更新

---
+++ hotReplace/webpack.config.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	devServer: {
+		hot: true
+	},
+	plugins:[
+		new webpack.HotModuleReplacementPlugin()
+	]
+};
